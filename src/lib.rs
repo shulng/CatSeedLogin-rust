cf1673fdaf9c1d62b7c7120b8c5ddde3798a8676
水灵 <1668589925@qq.com>
2025-12-01 01:28:28 -0800

    build: 初始化 Rust 项目结构
    添加 Cargo.toml 配置文件、基础库代码和 .gitignore 文件

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use pumpkin_api_macros::plugin_impl;
+
+#[plugin_impl]
+pub struct MyPlugin {}
+
+impl MyPlugin {
+    pub fn new() -> Self {
+        MyPlugin {}
+    }
+}
+
+impl Default for MyPlugin {
+    fn default() -> Self {
+        Self::new()
+    }
+}
